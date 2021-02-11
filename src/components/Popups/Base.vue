8ea134414bf709ccdb9b195081d48c1f62c15e24
HipyCas <hipycas@gmail.com>
2021-02-11 19:34:52 +0100

    Create Base component for popups
    Create a new Vue component, Base.vue, under components/popups/.
    This component is to be the base of all other popups.

---
+++ src/components/Popups/Base.vue
@@ -0,0 +1,22 @@
+<template>
+	<div
+		id="contentDiv"
+		class="z-50 fixed top-0 h-screen w-screen flex justify-center items-center"
+	>
+		<div class="w-9/12 h-4/5 h-max rounded-xl border-none bg-gray-50 p-5">
+			<slot></slot>
+		</div>
+	</div>
+	<div
+		id="backdrop"
+		class="z-40 fixed w-screen h-screen bg-gray-900 bg-opacity-50"
+	></div>
+</template>
+
+<script>
+export default {
+	name: 'PopupsBase',
+};
+</script>
+
+<style></style>
