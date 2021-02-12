1915ea0b5e156932c15edc1255e4f79b7588a077
HipyCas <hipycas@gmail.com>
2021-02-12 20:56:35 +0100

    Add GPL-3.0 license file and notice
    **THIS LICENSE APPLIES TO ANY PREVIOUS CODE OR RELEASE IN THIS CODEBASE**
    
    The license for this project is now set to GNU General Public License v3.0.
    The license file is created with the full license text, adapted to the project.
    A notice is also added to all code files in the codebase (.vue, .js, .css & .html).
    
    For any inquiries about the license change, please contact hipycas+socketchat@gmail.com

--- src/components/Popups/Base.vue
+++ src/components/Popups/Base.vue
@@ -1,9 +1,29 @@
+<!--
+  SocketChat: the FLOSS simple instant chat software built around socket.io
+  Copyright (C) 2021 Hipy Cas
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-->
+
 <template>
 	<div
 		id="contentDiv"
 		class="z-50 fixed top-0 h-screen w-screen flex justify-center items-center"
 	>
-		<div class="w-9/12 h-4/5 h-max rounded-xl border-none bg-gray-50 p-5 overflow-auto">
+		<div
+			class="w-9/12 h-4/5 h-max rounded-xl border-none bg-gray-50 p-5 overflow-auto"
+		>
 			<slot></slot>
 		</div>
 	</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,29 @@
+/*
+  SocketChat: the FLOSS simple instant chat software built around socket.io
+  Copyright (C) 2021 Hipy Cas
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-    animation: ['group-hover', 'hover', 'motion-safe']
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+		animation: ['group-hover', 'hover', 'motion-safe'],
+	},
+	plugins: [require('@tailwindcss/forms')],
 };
