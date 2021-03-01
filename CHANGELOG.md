38c1944a5299fd69a4133154ae1f8301cdff481b
HipyCas <hipycas@gmail.com>
2021-03-01 20:42:09 +0100

    Add GPLv3 badge to Welcom popup and start work on Changes popup
    In the Welcome popup, on the bottom-left, a GPLv3 badge/logo was added.
    This is due to the decision of licensing the project under GPLv3.
    
    Work has started on the Changes popup, which will show when the version changes.
    This will be made functional by storing the last version used in local storage and checking against the server.
    A CHANGELOG.md file has been also created, although it is not fully functional yet.

---
+++ CHANGELOG.md
@@ -0,0 +1,22 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+### Added
+
+- Welcome popup when you access the app, which summarizes the privacy, values and features of SocketChat
+- GPLv3 logo in bottom-left of welcome popup
+- Changes popup whenever the version of the app is different from the saved one in your computer
+
+## 1.0.0 - 2021-06-20
+
+### Added
+
+- Welcome popup when you access the app, which summarizes the privacy, values and features of SocketChat
+
+[unreleased]: https://github.com/HipyCas/SocketChat/compare/v1.0.0...HEAD
---
+++ src/components/Popups/Changes.vue
@@ -0,0 +1,36 @@
+<template>
+	<Base>
+		<h1
+			id="title"
+			class="w-full my-2 align-middle text-center text-2xl font-black text-yellow-500"
+		>
+			🎉 New features out! 🎉
+		</h1>
+		<p
+			id="subtitle"
+			class="w-full align-middle text-center px-5 text-gray-700 whitespace-pre-line"
+		>
+			SocketChat has been updated to version {{ newVersion }}, yay! The last
+			version you used was {{ oldVersion }}, but since then we have worked hard
+			and pushed so many awesome features! Here you have what has changesd since
+			you were gone:
+		</p>
+	</Base>
+</template>
+
+<script>
+import Base from './Base.vue';
+
+export default {
+	name: 'PopupsChanges',
+	components: { Base },
+	data() {
+		return {
+			newVersion: '1.0.0',
+			oldVersion: '0.8.4',
+		};
+	},
+};
+</script>
+
+<style></style>
